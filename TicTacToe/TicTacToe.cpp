929f416d632e13dcb27945380f0ad45ecce0feca
Christian Adame <adamechristian10@gmail.com>
2026-09-13 15:43:38 -0400

    Buster-3 Main Menu Function added

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,15 +1,32 @@
 #include <iostream>
 using namespace std;
 
+void MainMenu() {
+
+	int choice;
+
+	cout << "---------------------------" << endl;
+	cout << "|  Welcome to Tic-Tac-Toe |" << endl;
+	cout << "|-------------------------|" << endl;
+	cout << "| Please Select 1, 2 or 3 |" << endl;
+	cout << "|-------------------------|" << endl;
+	cout << "|         1. Play         |" << endl;
+	cout << "|      2. User Manual     |" << endl;
+	cout << "|         3. Exit         |" << endl;
+	cout << "---------------------------" << endl;
+
+	cin >> choice;
+
+}
 // Functions can be placed here, for example:
 // void displayBoard() {}
 
 
 
 int main() {
-	cout << "Welcome to Tic Tac Toe!" << endl;
-	// Additional game logic will go here
 
+	MainMenu();
+	// Additional game logic will go here
 
 
 	return 0;
