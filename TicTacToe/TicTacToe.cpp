1d1e0c240700d9921abc2a3e2ee90beedadeba1f
Hayden Nelson <haydenjnelson1@gmail.com>
2026-09-13 16:20:17 -0400

    Busters4 - Added gameplay

--- TicTacToe/TicTacToe.cpp
+++ TicTacToe/TicTacToe.cpp
@@ -1,33 +1,132 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
+
+class gameboard
+{
+public:
+    string cells[9] = { " ", " ", " ", " ", " ", " ", " ", " ", " " };
+    void display()
+    {
+        cout << " " << cells[0] << " | " << cells[1] << " | " << cells[2] << "\n-----------\n " << cells[3] << " | " << cells[4] << " | " << cells[5] << "\n-----------\n " << cells[6] << " | " << cells[7] << " | " << cells[8] << "\n";
+    }
+    void playerturn()
+    {
+        int playerchoice;
+        cout << "choose a cell (1-9): ";
+        cin >> playerchoice;
+        playerchoice--;
+        if (cells[playerchoice] != " ")
+        {
+            cout << "cell is already taken, try again.\n";
+            playerturn();
+        }
+        else
+        {
+            cells[playerchoice] = "x";
+        }
+    }
+    void cputurn()
+    {
+        int cpuchoice = rand() % 9;
+        if (cells[cpuchoice] != " ")
+        {
+            cputurn();
+        }
+        else
+        {
+            cout << "CPU choice: " << (cpuchoice + 1) << "\n";
+            cells[cpuchoice] = "o";
+        }
+    }
+    bool wincheck()
+    {
+        if (cells[0] == "x" && cells[1] == "x" && cells[2] == "x") { return true; }
+        else if (cells[3] == "x" && cells[4] == "x" && cells[5] == "x") { return true; }
+        else if (cells[6] == "x" && cells[7] == "x" && cells[8] == "x") { return true; }
+        else if (cells[0] == "x" && cells[3] == "x" && cells[6] == "x") { return true; }
+        else if (cells[1] == "x" && cells[4] == "x" && cells[7] == "x") { return true; }
+        else if (cells[2] == "x" && cells[5] == "x" && cells[8] == "x") { return true; }
+        else if (cells[0] == "x" && cells[4] == "x" && cells[8] == "x") { return true; }
+        else if (cells[2] == "x" && cells[4] == "x" && cells[6] == "x") { return true; }
+        else { return false; }
+    }
+    bool losecheck()
+    {
+        if (cells[0] == "o" && cells[1] == "o" && cells[2] == "o") { return true; }
+        else if (cells[3] == "o" && cells[4] == "o" && cells[5] == "o") { return true; }
+        else if (cells[6] == "o" && cells[7] == "o" && cells[8] == "o") { return true; }
+        else if (cells[0] == "o" && cells[3] == "o" && cells[6] == "o") { return true; }
+        else if (cells[1] == "o" && cells[4] == "o" && cells[7] == "o") { return true; }
+        else if (cells[2] == "o" && cells[5] == "o" && cells[8] == "o") { return true; }
+        else if (cells[0] == "o" && cells[4] == "o" && cells[8] == "o") { return true; }
+        else if (cells[2] == "o" && cells[4] == "o" && cells[6] == "o") { return true; }
+        else { return false; }
+    }
+};
+
+void playGame()
+{
+    srand(time(0));
+    gameboard game;
+    game.display();
+    int turns = 0;
+    bool gameover = false;
+    while (gameover == false)
+    {
+        game.playerturn();
+        turns++;
+        game.display();
+        if (game.wincheck() == true)
+        {
+            gameover = true;
+            cout << "\nGame Over, Player Wins!";
+            break;
+        }
+        if (turns >= 9)
+        {
+            gameover = true;
+            cout << "\nGame Over, It's a Tie!";
+            break;
+        }
+        game.cputurn();
+        turns++;
+        game.display();
+        if (game.losecheck() == true)
+        {
+            gameover = true;
+            cout << "\nGame Over, CPU Wins!";
+            break;
+        }
+
+    }
+}
 
 void MainMenu() {
 
-	int choice;
+    int choice;
 
-	cout << "---------------------------" << endl;
-	cout << "|  Welcome to Tic-Tac-Toe |" << endl;
-	cout << "|-------------------------|" << endl;
-	cout << "| Please Select 1, 2 or 3 |" << endl;
-	cout << "|-------------------------|" << endl;
-	cout << "|         1. Play         |" << endl;
-	cout << "|      2. User Manual     |" << endl;
-	cout << "|         3. Exit         |" << endl;
-	cout << "---------------------------" << endl;
+    cout << "---------------------------" << endl;
+    cout << "|  Welcome to Tic-Tac-Toe |" << endl;
+    cout << "|-------------------------|" << endl;
+    cout << "| Please Select 1, 2 or 3 |" << endl;
+    cout << "|-------------------------|" << endl;
+    cout << "|         1. Play         |" << endl;
+    cout << "|      2. User Manual     |" << endl;
+    cout << "|         3. Exit         |" << endl;
+    cout << "---------------------------" << endl;
 
-	cin >> choice;
+    cin >> choice;
+    if (choice == 1)
+    {
+        playGame();
+    }
 
 }
-// Functions can be placed here, for example:
-// void displayBoard() {}
 
-
-
-int main() {
-
-	MainMenu();
-	// Additional game logic will go here
-
-
-	return 0;
+int main()
+{
+    MainMenu();
 }
